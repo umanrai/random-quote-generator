4fb90c45741be84a2c3c492a1a243743f813cde0
umanrai <umanrai58@gmail.com>
2024-02-20 18:44:54 +0545

    Code Optimize

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-const text=document.getElementById("quote");
-const author=document.getElementById("author");
-const tweetButton=document.getElementById("tweet");
+const text = document.getElementById("quote");
+const author = document.getElementById("author");
+const tweetButton = document.getElementById("tweet");
 
-const getNewQuote = async () =>
-{
-    //api for quotes
-    var url="https://type.fit/api/quotes";    
+const getNewQuote = async () => {    
+    const response = await fetch("https://type.fit/api/quotes");
 
-    // fetch the data from api
-    const response=await fetch(url);
-    console.log(typeof response);
-    //convert response to json and store it in quotes array
     const allQuotes = await response.json();
 
-    // Generates a random number between 0 and the length of the quotes array
-    const indx = Math.floor(Math.random()*allQuotes.length);
+    const indx = Math.floor(Math.random() * allQuotes.length);
+    const quote = allQuotes[indx].text;
+    const authorName = allQuotes[indx].author;
 
-    //Store the quote present at the randomly generated index
-    const quote=allQuotes[indx].text;
+    // Ternary Operator
+    author.innerHTML = "~ " + (authorName == null ? "Anonymous" : authorName)
 
-    //Store the author of the respective quote
-    const auth=allQuotes[indx].author;
+    // if (authorName == null) {
+    //     author.innerHTML = "Anonymous";
+    // } else {
+    //     author.innerHTML = "~ " + authorName;
+    // }
 
-    if(auth==null)
-    {
-        author = "Anonymous";
-    }
-
-    //function to dynamically display the quote and the author
-    text.innerHTML=quote;
-    author.innerHTML="~ "+auth;
-
-    //tweet the quote
-    tweetButton.href="https://twitter.com/intent/tweet?text="+quote+" ~ "+auth;
+    text.innerHTML = quote;
+    tweetButton.href = "https://twitter.com/intent/tweet?text=" + quote + " ~ " + authorName;
 }
+
 getNewQuote();
