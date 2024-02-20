8a09562dfac4695e36c77a7a1c0139edb0335a96
umanrai <umanrai58@gmail.com>
2024-02-20 19:35:23 +0545

    Changed old Api to new Api
    https://type.fit/api/quotes to https://zenquotes.io/api/quotes

--- script.js
+++ script.js
@@ -2,26 +2,37 @@ const text = document.getElementById("quote");
 const author = document.getElementById("author");
 const tweetButton = document.getElementById("tweet");
 
+let allQuotes = [];
+
 const getNewQuote = async () => {    
-    const response = await fetch("https://type.fit/api/quotes");
+    const response = await fetch("https://zenquotes.io/api/quotes");
+    
+    allQuotes = await response.json();
 
-    const allQuotes = await response.json();
+    showRandomQuote()
+}
 
+getNewQuote();
+
+function showRandomQuote() {
     const indx = Math.floor(Math.random() * allQuotes.length);
-    const quote = allQuotes[indx].text;
-    const authorName = allQuotes[indx].author;
+    
+    const quote = allQuotes[indx].q;
+    const authorName = allQuotes[indx].a;
 
-    // Ternary Operator
+    //Ternary Operator
     author.innerHTML = "~ " + (authorName == null ? "Anonymous" : authorName)
 
-    // if (authorName == null) {
-    //     author.innerHTML = "Anonymous";
-    // } else {
-    //     author.innerHTML = "~ " + authorName;
-    // }
+    if (authorName == null) {
+        author.innerHTML = "Anonymous";
+    } else {
+        author.innerHTML = "~ " + authorName;
+    }
 
     text.innerHTML = quote;
     tweetButton.href = "https://twitter.com/intent/tweet?text=" + quote + " ~ " + authorName;
 }
 
-getNewQuote();
+var elements = document.getElementById('next-quota').addEventListener('click', function() {
+    showRandomQuote()
+});
