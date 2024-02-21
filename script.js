156bf4456233fc56f73b2efbeeec2083ac97f704
umanrai <umanrai58@gmail.com>
2024-02-21 22:02:11 +0545

    Added copy to clipboard and enhanced design #1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const text = document.getElementById("quote");
 const author = document.getElementById("author");
 const tweetButton = document.getElementById("tweet");
+const fbButton = document.getElementById("facebook");
+
 
 let allQuotes = [];
 
@@ -30,9 +32,47 @@ function showRandomQuote() {
     }
 
     text.innerHTML = quote;
-    tweetButton.href = "https://twitter.com/intent/tweet?text=" + quote + " ~ " + authorName;
+    
+    // tweetButton.href = "https://twitter.com/intent/tweet?text=" + quote + " ~ " + authorName;
+    tweetButton.href = `https://twitter.com/intent/tweet?text=${quote} ~ ${authorName}`
+    // fbButton.href = "https://www.facebook.com/sharer/sharer.php?u=" + window.location.href + "&quote=" + quote;
+    fbButton.href = `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}&quote=${quote}`
 }
 
-var elements = document.getElementById('next-quota').addEventListener('click', function() {
+document.getElementById('next-quota').addEventListener('click', function() {
     showRandomQuote()
 });
+
+document.getElementById('myTooltip').addEventListener('click', function () {
+    var tempTextArea = document.createElement('textarea');
+
+    tempTextArea.value = document.querySelector('.quote').innerHTML;
+
+    document.body.appendChild(tempTextArea);
+
+    tempTextArea.select();
+
+    document.execCommand('copy');
+
+    document.body.removeChild(tempTextArea);
+
+    document.querySelector(".tooltiptext").innerHTML = 'Copied'
+})
+
+
+// document.getElementById('myTooltip').addEventListener('mouseout', function () {
+//     console.log('mouse out');
+//     // document.querySelector(".tooltiptext").innerHTML = 'Copy to clipboard'
+// })
+
+document.getElementById('myTooltip').addEventListener('mouseenter', function () {
+    document.querySelector(".tooltiptext").innerHTML = 'Copy to clipboard'
+})
+
+document.querySelector('.container').addEventListener('mouseover', function () {
+    document.querySelector(".tooltip").style.visibility = 'visible'
+})
+
+document.querySelector('.container').addEventListener('mouseout', function () {
+    document.querySelector(".tooltip").style.visibility = 'hidden'
+})
